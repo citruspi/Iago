f2674b732a47b32a6ab5a91a3ee1739f557d6510
Mihir Singh <git.service@mihirsingh.com>
2015-02-21 23:13:44 -0600

    Separated the handler functions from the router binding call

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ type Status struct {
 var (
 	hosts []Host
 	TTL   int64
+	boot  time.Time
 )
 
 func buildExpiration() time.Time {
@@ -64,8 +65,86 @@ func cleanup() {
 	}
 }
 
+func announce(c *gin.Context) {
+	var notification travis.Notification
+
+	c.Bind(&notification)
+
+	payload, _ := json.Marshal(notification.Payload)
+	body := bytes.NewBuffer(payload)
+
+	for _, host := range hosts {
+		urlStr := buildURL(host)
+
+		req, err := http.NewRequest("POST", urlStr, body)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer resp.Body.Close()
+	}
+
+	c.JSON(200, "")
+}
+
+func checkin(c *gin.Context) {
+	var host Host
+
+	c.Bind(&host)
+
+	if host.Hostname != "" {
+		for i, h := range hosts {
+			if h.Hostname == host.Hostname {
+				diff := hosts
+				diff = append(diff[:i], diff[i+1:]...)
+				hosts = diff
+			}
+		}
+
+		if host.Protocol == "" {
+			host.Protocol = "http"
+		}
+
+		if host.Port == 0 {
+			if host.Protocol == "http" {
+				host.Port = 80
+			} else if host.Protocol == "https" {
+				host.Port = 443
+			}
+		}
+
+		if host.Path == "" {
+			host.Path = "/"
+		}
+
+		host.Expiration = buildExpiration()
+		hosts = append(hosts, host)
+
+		c.JSON(200, "")
+	} else {
+		var response struct {
+			Message string `json:"message"`
+		}
+		response.Message = "Invalid hostname."
+		c.JSON(400, response)
+	}
+}
+
+func status(c *gin.Context) {
+	status := Status{}
+	status.Hosts = hosts
+	status.Uptime = time.Now().Sub(boot).Seconds()
+
+	c.JSON(200, status)
+}
+
 func main() {
-	boot := time.Now()
+	boot = time.Now()
 
 	config, err := mini.LoadConfiguration("iago.ini")
 
@@ -79,83 +158,9 @@ func main() {
 
 	router := gin.Default()
 
-	router.POST("/checkin/", func(c *gin.Context) {
-		var host Host
-
-		c.Bind(&host)
-
-		if host.Hostname != "" {
-			for i, h := range hosts {
-				if h.Hostname == host.Hostname {
-					diff := hosts
-					diff = append(diff[:i], diff[i+1:]...)
-					hosts = diff
-				}
-			}
-
-			if host.Protocol == "" {
-				host.Protocol = "http"
-			}
-
-			if host.Port == 0 {
-				if host.Protocol == "http" {
-					host.Port = 80
-				} else if host.Protocol == "https" {
-					host.Port = 443
-				}
-			}
-
-			if host.Path == "" {
-				host.Path = "/"
-			}
-
-			host.Expiration = buildExpiration()
-			hosts = append(hosts, host)
-
-			c.JSON(200, "")
-		} else {
-			var response struct {
-				Message string `json:"message"`
-			}
-			response.Message = "Invalid hostname."
-			c.JSON(400, response)
-		}
-	})
-
-	router.POST("/announce/", func(c *gin.Context) {
-		var notification travis.Notification
-
-		c.Bind(&notification)
-
-		payload, _ := json.Marshal(notification.Payload)
-		body := bytes.NewBuffer(payload)
-
-		for _, host := range hosts {
-			urlStr := buildURL(host)
-
-			req, err := http.NewRequest("POST", urlStr, body)
-			req.Header.Set("Content-Type", "application/json")
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			defer resp.Body.Close()
-		}
-
-		c.JSON(200, "")
-	})
-
-	router.GET("/status/", func(c *gin.Context) {
-		status := Status{}
-		status.Hosts = hosts
-		status.Uptime = time.Now().Sub(boot).Seconds()
-
-		c.JSON(200, status)
-	})
+	router.POST("/checkin/", checkin)
+	router.POST("/announce/", announce)
+	router.GET("/status/", status)
 
 	address := config.StringFromSection("Web", "Address", "127.0.0.1:8080")
 
