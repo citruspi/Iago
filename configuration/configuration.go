e0d490336bc9937726970040c37a12fbf45ed130
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 16:29:37 -0600

    Implemented parsing of the Iago configuration section

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -7,6 +7,17 @@ import (
 	"github.com/FogCreek/mini"
 )
 
+type iagoConfiguration struct {
+	Hostname string
+	Protocol string
+	Port     string
+	Path     string
+}
+
+var (
+	Iago iagoConfiguration
+)
+
 func Process() {
 	path := flag.String("config", "/etc/miloud.ini", "Configuration file path")
 	flag.Parse()
@@ -15,5 +26,18 @@ func Process() {
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	Iago.Hostname = config.StringFromSection("Iago", "Hostname", "")
+	Iago.Protocol = config.StringFromSection("Iago", "Protocol", "http")
+	Iago.Path = config.StringFromSection("Iago", "Path", "/")
+	Iago.Port = config.IntegerFromSection("Iago", "Port", 0)
+
+	if Iago.Port == 0 {
+		if Iago.Protocol == "http" {
+			Iago.Port = 80
+		} else if Iago.Protocol == "https" {
+			Iago.Port = 443
+		}
 	}
 }
