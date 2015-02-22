836dfa3a5992d3b97a30d60786be3fc0d91e35a9
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 17:37:51 -0600

    Invoked the Iago checkin function as a go routine

--- miloud/main.go
+++ miloud/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	conf "github.com/citruspi/Milou/configuration"
 	"github.com/citruspi/Milou/handlers"
+	"github.com/citruspi/Milou/iago"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	conf.Process()
+
+	go iago.CheckIn()
 
 	router := gin.Default()
 
