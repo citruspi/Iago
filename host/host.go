ddd5e873c4e5994549ba2222b9075b396112654d
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 14:30:10 -0600

    Implemented signing notifiations with ECDSA

--- host/host.go
+++ host/host.go
@@ -2,7 +2,12 @@ package host
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/x509"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -21,10 +26,16 @@ type Host struct {
 }
 
 type Notification struct {
-	Repository string `json:"repository"`
-	Owner      string `json:"owner"`
-	Commit     string `json:"commit"`
-	Branch     string `json:"branch"`
+	Repository string    `json:"repository"`
+	Owner      string    `json:"owner"`
+	Commit     string    `json:"commit"`
+	Branch     string    `json:"branch"`
+	Signature  Signature `json:"signature"`
+}
+
+type Signature struct {
+	S string `json:"s"`
+	R string `json:"r"`
 }
 
 var (
@@ -90,12 +101,52 @@ func Cleanup() {
 	}
 }
 
+func (notification Notification) Sign() Notification {
+	privateKeyRaw, err := ioutil.ReadFile("key")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := x509.ParseECPrivateKey(privateKeyRaw)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var message bytes.Buffer
+	message.WriteString(notification.Owner)
+	message.WriteString("/")
+	message.WriteString(notification.Repository)
+	message.WriteString("@")
+	message.WriteString(notification.Branch)
+	message.WriteString("#")
+	message.WriteString(notification.Commit)
+
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey, message.Bytes())
+
+	fmt.Println(r)
+	fmt.Println(s)
+	fmt.Println(string(message.Bytes()))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	notification.Signature.R = r.String()
+	notification.Signature.S = s.String()
+
+	return notification
+}
+
 func Notify(announcement travis.Announcement) {
 	notification := Notification{}
 	notification.Repository = announcement.Payload.Repository.Name
 	notification.Owner = announcement.Payload.Repository.Owner
 	notification.Commit = announcement.Payload.Commit
 	notification.Branch = announcement.Payload.Branch
+
+	notification = notification.Sign()
 
 	content, _ := json.Marshal(notification)
 	body := bytes.NewBuffer(content)
