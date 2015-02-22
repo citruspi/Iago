07ae9ae04d43155a326b9775a2d2bf5637124138
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 17:52:27 -0600

    Verified notification signatures when signed is true

--- handlers/notification.go
+++ handlers/notification.go
@@ -1,9 +1,23 @@
 package handlers
 
 import (
+	"github.com/citruspi/Iago/notification"
+	conf "github.com/citruspi/Milou/configuration"
 	"github.com/gin-gonic/gin"
 )
 
 func Notification(c *gin.Context) {
-	c.String(200, "yolo")
+	var n notification.Notification
+
+	c.Bind(&n)
+
+	if conf.Notification.Signed {
+		if n.Verify(conf.Notification.PublicKey) {
+			c.JSON(200, "")
+		} else {
+			c.JSON(400, "")
+		}
+	} else {
+		c.JSON(200, "")
+	}
 }
