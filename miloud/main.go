dc4a5b0546921f8da8529a4f9c80b462f3cc08a6
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 17:35:30 -0600

    Implemented a handler to receive notifications

--- miloud/main.go
+++ miloud/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	conf "github.com/citruspi/Milou/configuration"
+	"github.com/citruspi/Milou/handlers"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	conf.Process()
+
+	router := gin.Default()
+
+	router.POST("/", handlers.Notification)
+
+	router.Run(conf.Web.Address)
 }
