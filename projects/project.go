5cc2db70ac0cf8b2dc895583213363ff53f5ebba
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 18:20:47 -0600

    Defined a function which returns the end destination for the project

--- projects/project.go
+++ projects/project.go
@@ -10,3 +10,17 @@ type Project struct {
 	Subdomain  string
 	Type       string
 }
+
+func (p Project) Path() string {
+	var buffer bytes.Buffer
+
+	if p.Type == "static" {
+		buffer.WriteString("/srv/")
+		buffer.WriteString(p.Domain)
+		buffer.WriteString("/")
+		buffer.WriteString(p.Subdomain)
+		buffer.WriteString("/")
+	}
+
+	return string(buffer.Bytes())
+}
