eeda802ddc718097a5f4e5a27a2e2a4bcc86110c
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 22:11:03 -0600

    On startup, automatically deploy every project

--- miloud/main.go
+++ miloud/main.go
@@ -4,11 +4,16 @@ import (
 	conf "github.com/citruspi/Milou/configuration"
 	"github.com/citruspi/Milou/handlers"
 	"github.com/citruspi/Milou/iago"
+	"github.com/citruspi/Milou/projects"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	conf.Process()
+
+	for _, project := range projects.List {
+		project.Deploy()
+	}
 
 	go iago.CheckIn()
 
