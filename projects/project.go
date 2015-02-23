dde5571f54014799860c323153a67e62db95357e
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 18:09:14 -0600

    Defined the project type

---
+++ projects/project.go
@@ -0,0 +1,12 @@
+package projects
+
+type Project struct {
+	Name       string
+	Owner      string
+	Repository string
+	Version    string
+	Identifier string
+	Domain     string
+	Subdomain  string
+	Type       string
+}
