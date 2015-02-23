588e0d18b88710f38f72449f08cdd6243d129ec0
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 21:59:54 -0600

    Invoked the projects.Process function as a go routine when a notification is received

--- handlers/notification.go
+++ handlers/notification.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/citruspi/Iago/notification"
 	conf "github.com/citruspi/Milou/configuration"
+	"github.com/citruspi/Milou/projects"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,7 @@ func Notification(c *gin.Context) {
 
 	if conf.Notification.Signed {
 		if n.Verify(conf.Notification.PublicKey) {
+			go projects.Process(n)
 			c.JSON(200, "")
 		} else {
 			c.JSON(400, "")
