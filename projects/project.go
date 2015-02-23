c0352acfe12b024371be6dcc74c22f94249bef75
Mihir Singh <git.service@mihirsingh.com>
2015-02-23 08:16:00 -0600

    Allowed the user to specify a branch or commit for the version

--- projects/project.go
+++ projects/project.go
@@ -187,8 +187,14 @@ func Process(n notification.Notification) {
 	for _, project := range List {
 		if project.Repository == n.Repository {
 			if project.Owner == n.Owner {
-				if (project.Version == n.Branch) || (project.Version == n.Commit) {
-					project.Deploy()
+				if project.Version[1] == "#" {
+					if project.Version[:len(project.Version)-1] == n.Commit {
+						project.Deploy()
+					}
+				} else if project.Version[1] == "@" {
+					if project.Version[:len(project.Version)-1] == n.Branch {
+						project.Deploy()
+					}
 				}
 			}
 		}
