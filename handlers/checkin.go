928f615b33af2633469dceb8d28d2f4560f2ac23
Mihir Singh <git.service@mihirsingh.com>
2015-03-07 22:07:01 -0600

    Added logging for the checkin endpoint

--- handlers/checkin.go
+++ handlers/checkin.go
@@ -1,20 +1,29 @@
 package handlers
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/citruspi/iago/host"
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+}
 
 func CheckIn(c *gin.Context) {
 	var h host.Host
 
 	c.Bind(&h)
 
+	log.Info("Received host checkin")
+
 	if h.Hostname != "" {
 		h.CheckIn()
 
 		c.JSON(200, "")
 	} else {
+		log.Error("Host is incomplete")
+
 		var response struct {
 			Message string `json:"message"`
 		}
