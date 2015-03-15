0eef4187ed41289ac535b71b11de3ec7a69962ef
Mihir Singh <git.service@mihirsingh.com>
2015-03-15 00:42:00 -0500

    Implemented a Subscribe() function on the pubsub package

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/citruspi/milou/configuration"
 	"github.com/citruspi/milou/handlers"
 	"github.com/citruspi/milou/notifications"
 	"github.com/citruspi/milou/projects"
-	"github.com/fzzy/radix/extra/pubsub"
-	"github.com/fzzy/radix/redis"
+	"github.com/citruspi/milou/pubsub"
 )
 
 var (
@@ -32,27 +30,27 @@ func main() {
 		http.HandleFunc("/", handlers.Travis)
 		http.ListenAndServe(conf.Web.Address, nil)
 	} else if conf.Mode == "client" {
-		timeout := time.Duration(conf.Redis.Timeout) * time.Second
+		projects.DeployAll()
 
-		conn, err := redis.DialTimeout("tcp", conf.Redis.Address, timeout)
+		var channels []string
+
+		for _, project := range projects.Projects {
+			channels = append(channels, "milou."+project.Owner+"."+project.Repository)
+		}
+
+		client, err := pubsub.Subscribe(channels)
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		defer conn.Close()
-
-		psc := pubsub.NewSubClient(conn)
-
-		projects.DeployAll()
-
 		for {
-			psr := psc.Receive()
+			reply := client.Receive()
 
-			if !psr.Timeout() {
+			if !reply.Timeout() {
 				var notification notifications.Notification
 
-				err = json.Unmarshal([]byte(psr.Message), &notification)
+				err = json.Unmarshal([]byte(reply.Message), &notification)
 
 				if err != nil {
 					log.Fatal(err)
---
+++ pubsub/sub.go
@@ -0,0 +1,40 @@
+package pubsub
+
+import (
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/fzzy/radix/extra/pubsub"
+	"github.com/fzzy/radix/redis"
+)
+
+func Subscribe(channels []string) (*pubsub.SubClient, error) {
+	var err error
+	var conn *redis.Client
+	var timeout time.Duration
+	var client *pubsub.SubClient
+
+	timeout = time.Duration(conf.Redis.Timeout) * time.Second
+
+	conn, err = redis.DialTimeout("tcp", conf.Redis.Address, timeout)
+
+	if err != nil {
+		return client, err
+	} else {
+		log.Info("Established connection to redis")
+	}
+
+	defer conn.Close()
+
+	client = pubsub.NewSubClient(conn)
+
+	for _, channel := range channels {
+		r := client.Subscribe(channel)
+
+		if r.Err != nil {
+			return client, r.Err
+		}
+	}
+
+	return client, nil
+}
